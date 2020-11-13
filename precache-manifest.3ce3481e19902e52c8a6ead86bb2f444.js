d5ff1c3c7eb9232e2257c9a9004e4180a093b5ef
hilary lin <hilary.lin@keeda.com.tw>
2020-11-13 11:57:13 +0800

    Updates

---
+++ precache-manifest.3ce3481e19902e52c8a6ead86bb2f444.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588037c667a5abbde4abe590cc5c0ecc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "96d8054ac1b31a909bf6",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "93b05b3e2951da67f9ed",
+    "url": "./static/js/2.1a33e061.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.1a33e061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96d8054ac1b31a909bf6",
+    "url": "./static/js/main.501da99f.chunk.js"
+  },
+  {
+    "revision": "e646532253b3e4cf99e8",
+    "url": "./static/js/runtime-main.dc857f5b.js"
+  }
+]);
